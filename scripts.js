f603f1a424bf6c75cf7fbf9f922c8454aecb81a0
pamela sindy <pamelab.desire@gmail.com>
2019-07-19 11:34:03 +0200

    add the file in the project2

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function AkanName() {
+    var gender;
+    var DD = parseFloat(document.getElementById("dd").value);
+    var MM = parseFloat(document.getElementById("mm").value);
+    var YY = parseFloat(document.getElementById("yy").value);
+    var CC = (YY - 1) / 100 + 1;
+    var arrayofDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var dayoftheweek = (CC / 4 - 2 * CC - 1 + 5 * YY / 4 + (26 * (MM + 1) / 10) + DD) % 7;
+    console.log(dayoftheweek);
+    document.getElementById('demo').innerHTML = Math.round(dayoftheweek);
+}
