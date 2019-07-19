37fe200413cbf55e1191269f5aa50a5b06d02bbc
pamela sindy <pamelab.desire@gmail.com>
2019-07-19 13:07:10 +0200

    add the value in script

--- scripts.js
+++ scripts.js
@@ -8,4 +8,11 @@ function AkanName() {
     var dayoftheweek = (CC / 4 - 2 * CC - 1 + 5 * YY / 4 + (26 * (MM + 1) / 10) + DD) % 7;
     console.log(dayoftheweek);
     document.getElementById('demo').innerHTML = Math.round(dayoftheweek);
+    var arrayoffemale = ["Akosua", "Aduoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var arrayofmale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var gender = document.getElementById(input[name=gender]).value;
+    if (gender == 'female') {
+        document.getElementById('demo').innerHTML = arrayoffemale[Math.round(dayoftheweek)];
+        document.getElementById('demo').innerHTML = arrayofmale[Math.round(dayoftheweek)];
+    }
 }
